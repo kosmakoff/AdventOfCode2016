d30861bdb113aec808f6196f61f30d237091b7d4
Oleg Kosmakov <kosmakoff@gmail.com>
2016-12-11 00:33:24 +0000

    Day 6 complete

---
+++ Day06/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Advent Of Code 2016, day 6");
+
+            var fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("File not found");
+                return;
+            }
+
+            var input = File.ReadAllText(fileName);
+
+            var datas = input.Split(new [] {'\n'}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var length = datas.First().Length;
+
+            var sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                var commonLetter = datas.Select(d => d[i])
+                    .GroupBy(c => c)
+                    .OrderByDescending(grp => grp.Count())
+                    .Select(grp => grp.Key)
+                    .First();
+
+                sb.Append(commonLetter);
+            }
+
+            Console.WriteLine($"The message is '{sb}'");
+
+            sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                var commonLetter = datas.Select(d => d[i])
+                    .GroupBy(c => c)
+                    .OrderBy(grp => grp.Count())
+                    .Select(grp => grp.Key)
+                    .First();
+
+                sb.Append(commonLetter);
+            }
+
+            Console.WriteLine($"The REAL message is '{sb}'");
+        }
+    }
+}
---
+++ Day06/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day06")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c58f04a-112b-4b14-a07f-e3f171409d7e")]
