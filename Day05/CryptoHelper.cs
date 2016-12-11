c0b7dc396e2dd14681c378ce0900c5ca51bfd744
Oleg Kosmakov <kosmakoff@gmail.com>
2016-12-11 00:05:54 +0000

    Day 5 complete

---
+++ Day05/CryptoHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    public static class CryptoHelper
+    {
+        private static readonly MD5 Md5;
+
+        static CryptoHelper()
+        {
+            Md5 = MD5.Create();
+        }
+
+        public static string GetMd5Hash(string input)
+        {
+            var bytes = Encoding.ASCII.GetBytes(input);
+            var md5 = Md5.ComputeHash(bytes);
+            return BytesToString(md5);
+        }
+
+        public static string BytesToString(byte[] bytes)
+        {
+            var sb = new StringBuilder(bytes.Length * 2);
+
+            foreach (var b in bytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Advent Of Code 2016, day 5");
+
+            const string input = "abbhdwsy";
+
+            var counter = 0;
+
+            var sb = new StringBuilder(8);
+
+            for (int i = 0; counter <8 && i < int.MaxValue; i++)
+            {
+                var subInput = $"{input}{i}";
+                var md5 = CryptoHelper.GetMd5Hash(subInput);
+                if (!md5.StartsWith("00000"))
+                    continue;
+
+                sb.Append(md5[5]);
+                counter++;
+            }
+
+            Console.WriteLine($"The first door password is {sb}");
+
+            sb = new StringBuilder(new string(' ', 8));
+            var mem = new BitArray(8, false);
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var subInput = $"{input}{i}";
+                var md5 = CryptoHelper.GetMd5Hash(subInput);
+                if (!md5.StartsWith("00000"))
+                    continue;
+
+                var position = md5[5] - '0';
+
+                if (position > 7)
+                    continue;
+
+                var c = md5[6];
+
+                if (!mem.Get(position))
+                {
+                    sb[position] = c;
+                    mem.Set(position, true);
+
+                    if (mem.Cast<bool>().All(s => s))
+                        break;
+                }
+            }
+
+            Console.WriteLine($"The second door password is {sb}");
+        }
+    }
+}
